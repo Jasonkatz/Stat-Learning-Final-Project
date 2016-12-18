671ca56870d730823fa4e8e12b930efb60bb50c3
Jason Katz <jkatz94@gmail.com>
2016-12-18 16:09:52 -0500

    Got LDA working

--- query_data.sh
+++ query_data.sh
@@ -2,6 +2,6 @@
 
 clear
 
-echo "Running node processData.js \#whywomendontreport \#notokay \#masculinitysofragile \#whyistayed \#rapecultureiswhen \#webelieveyou \#tihty \#tilithappenstoyou \#askingforit \#thehuntingground \#rapeculture \#listentosurvivors \#thingslongerthanbrockturnersrapesentense \#endrapeculture \#stanfordrapist \#sexualviolence \#emilydoe"
+echo "Running node processData.js \#whywomendontreport \#notokay \#masculinitysofragile \#whyistayed \#rapecultureiswhen \#webelieveyou \#tihty \#tilithappenstoyou \#askingforit \#thehuntingground \#rapeculture \#listentosurvivors \#thingslongerthanbrockturnersrapesentence \#endrapeculture \#stanfordrapist \#sexualviolence \#emilydoe"
 
-node processData.js \#whywomendontreport \#notokay \#masculinitysofragile \#whyistayed \#rapecultureiswhen \#webelieveyou \#tihty \#tilithappenstoyou \#askingforit \#thehuntingground \#rapeculture \#listentosurvivors \#thingslongerthanbrockturnersrapesentense \#endrapeculture \#stanfordrapist \#sexualviolence \#emilydoe
+node processData.js \#whywomendontreport \#notokay \#masculinitysofragile \#whyistayed \#rapecultureiswhen \#webelieveyou \#tihty \#tilithappenstoyou \#askingforit \#thehuntingground \#rapeculture \#listentosurvivors \#thingslongerthanbrockturnersrapesentence \#endrapeculture \#stanfordrapist \#sexualviolence \#emilydoe
